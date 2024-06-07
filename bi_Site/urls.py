41f6a9d2d81bbc0f677a55f6fb7ab8e6867299ab
Javon Browne <javonbrowne@gmail.com>
2024-06-07 13:27:43 -0400

    Configured new views

--- bi_Site/urls.py
+++ bi_Site/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
