85f8c906e669e4fdacdffc21217fce6456dd21c2
Javon Browne <javonbrowne@gmail.com>
2024-08-21 21:49:58 -0400

    services model

--- bi_Site/models.py
+++ bi_Site/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+# Create your models here
+class Intro(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='services')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+class Meta:
+    ordering = ['title']
+    verbose_name_plural = 'Services'
