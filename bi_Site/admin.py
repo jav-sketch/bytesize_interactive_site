1cddc050b3206b570faae30343e0c08ee75960b0
Javon Browne <javonbrowne@gmail.com>
2024-08-23 12:07:06 -0400

    Added new models

--- bi_Site/admin.py
+++ bi_Site/admin.py
@@ -4,3 +4,9 @@ from .models import *
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Intro)
+# admin.site.register(Featurette)
+@admin.register(Featurette)
+class FeaturetteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image')
+    list_filter = ('is_active', 'date_created')
+    search_fields = ('title', 'description')
