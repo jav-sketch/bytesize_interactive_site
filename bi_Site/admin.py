ef4d3d0ada7438db3fa375e6897f2b3f56bce7ce
Javon Browne <javonbrowne@gmail.com>
2024-08-23 12:22:29 -0400

    Updated registered models

--- bi_Site/admin.py
+++ bi_Site/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Service)
-admin.site.register(Intro)
+# admin.site.register(Service)
+# admin.site.register(Intro)
 # admin.site.register(Featurette)
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description', 'image', 'date_created', 'date_updated')
+    list_filter = ( 'date_created', 'date_updated')
+    search_fields = ('title', 'description')
+
+@admin.register(Intro)
+class IntroAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name',)
+    search_fields = ('name', 'description')
+
+
 @admin.register(Featurette)
 class FeaturetteAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'image')
