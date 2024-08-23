b66ac00b5ad4455f5226c59e0f7b5beb84fc599c
Javon Browne <javonbrowne@gmail.com>
2024-08-23 12:08:48 -0400

    Running Tests

--- bi_Site/tests.py
+++ bi_Site/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from bi_Site.models import *
+from django.contrib.auth.models import User
+
+class Service(TestCase):
+    def setup(self):
+        self.service = Service.objects.create(
+            title = 'Test Service',
+            description = 'This is a test service',
+            image = 'test_image.jpg'
+        )
+    
+    def test_service_creation(self):
+        self.assertEqual(self.service.title, 'Test Service')
+        self.assertEqual(self.service.description, 'This is a test service')
