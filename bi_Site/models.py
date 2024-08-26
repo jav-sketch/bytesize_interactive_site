61e4c376823ef572d140eec6c1c55777d22bdb64
Javon Browne <javonbrowne@gmail.com>
2024-08-26 13:48:42 -0400

    Added about model

--- bi_Site/models.py
+++ bi_Site/models.py
@@ -48,3 +48,13 @@ class Featurette(models.Model):
 
     def __str__(self):
         return self.title
+
+class About(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
