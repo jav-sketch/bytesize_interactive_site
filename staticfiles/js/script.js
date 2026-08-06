7a068b2295582b7266b299d0d253d406da4d7421
Javon Browne <javonbrowne@gmail.com>
2026-08-06 16:14:21 -0400

    deleted staticfiles directory

--- staticfiles/js/script.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll('.nav-link');
-const windowPath = window.location.pathname;
-
-navLinks.forEach(navLink => {
-    if (navLink.href === window.location.href) {
-        navLink.classList.add('active');
-    }
-});
